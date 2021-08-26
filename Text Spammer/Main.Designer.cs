e8856adb3b76ffc2f16fe2cded8fc3b749968a60
Rubin <68704042+0xRubin@users.noreply.github.com>
2021-08-26 10:38:56 -0700

    Add project files.

---
+++ Text Spammer/Main.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Text_Spammer
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exit = new System.Windows.Forms.Label();
+            this.mini = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // exit
+            // 
+            this.exit.AutoSize = true;
+            this.exit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.exit.Location = new System.Drawing.Point(239, 9);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(14, 15);
+            this.exit.TabIndex = 0;
+            this.exit.Text = "X";
+            this.exit.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // mini
+            // 
+            this.mini.AutoSize = true;
+            this.mini.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.mini.Location = new System.Drawing.Point(221, 9);
+            this.mini.Name = "mini";
+            this.mini.Size = new System.Drawing.Size(12, 15);
+            this.mini.TabIndex = 1;
+            this.mini.Text = "-";
+            this.mini.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.mini.Click += new System.EventHandler(this.mini_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(245, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(182, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Credits";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(23, 80);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(119, 19);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Normal spammer";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(148, 80);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(100, 19);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "GTA spammer";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(23, 105);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(122, 19);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "Valorant spammer";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(148, 105);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(106, 19);
+            this.checkBox4.TabIndex = 7;
+            this.checkBox4.Text = "Apex spammer";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(269, 159);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.mini);
+            this.Controls.Add(this.exit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Main";
+            this.Text = "Text Spammer";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Main_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Main_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label exit;
+        private System.Windows.Forms.Label mini;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+    }
+}
---
+++ Text Spammer/Main.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Text_Spammer
+{
+    public partial class Main : Form
+    {
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(Keys vKey);
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+    (
+        int nLeftRect,
+        int nTopRect,
+        int nRightRect,
+        int nBottomRect,
+        int nWidthEllipse,
+        int nHeightEllipse
+    );
+        Point lastPoint;
+        public Main()
+        {
+            InitializeComponent();
+            //Rounded Corners
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 18, 18));
+        }
+        //Exit Form
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        //Minimize Window
+        private void mini_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+        //Draggable Form
+        private void Main_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void Main_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+        //Text Spammer
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                if (checkBox1.Checked)
+                {
+                    if (GetAsyncKeyState(Keys.XButton1) < 0)
+                    {
+                        try
+                        {
+                            if (textBox1.Text == "")
+                            {
+                                MessageBox.Show("No Text!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            };
+                            if (checkBox2.Checked)
+                            {
+                                MessageBox.Show("Another spammer is active(2)!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            };
+                            if (checkBox3.Checked)
+                            {
+                                MessageBox.Show("Another spammer is active(3)!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            };
+                            if (checkBox4.Checked)
+                            {
+                                MessageBox.Show("Another spammer is active(4)", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            }
+                            else
+                            {
+                                SendKeys.SendWait(textBox1.Text + "{ENTER}");
+                                Thread.Sleep(250);
+                            }
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+                }
+                Thread.Sleep(10);
+
+                if (checkBox2.Checked)
+                {
+                    if (GetAsyncKeyState(Keys.XButton1) < 0)
+                    {
+                        try
+
+                        {
+                            if (textBox1.Text == "")
+                            {
+                                MessageBox.Show("No Text!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            };
+                            if (checkBox1.Checked)
+                            {
+                                MessageBox.Show("Another spammer is active(1)!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            };
+                            if (checkBox3.Checked)
+                            {
+                                MessageBox.Show("Another spammer is active(3)!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            };
+                            if (checkBox4.Checked)
+                            {
+                                MessageBox.Show("Another spammer is active(4)", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            }
+                            else
+                            {
+                                SendKeys.SendWait("{T}" + textBox1.Text + "{ENTER}");
+                                Thread.Sleep(150);
+                            }
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+                }
+                Thread.Sleep(10);
+
+                if (checkBox3.Checked)
+                {
+                    if (GetAsyncKeyState(Keys.XButton1) < 0)
+                    {
+                        try
+                        {
+                            if (textBox1.Text == "")
+                            {
+                                MessageBox.Show("No Text!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            };
+                            if (checkBox1.Checked)
+                            {
+                                MessageBox.Show("Another spammer is active(1)!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            };
+                            if (checkBox2.Checked)
+                            {
+                                MessageBox.Show("Another spammer is active(2)!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            };
+                            if (checkBox4.Checked)
+                            {
+                                MessageBox.Show("Another spammer is active(4)", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            }
+                            else
+                            {
+                                SendKeys.SendWait("{Y}" + textBox1.Text + "{ENTER}");
+                                Thread.Sleep(150);
+                            }
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+                }
+                Thread.Sleep(10);
+
+                if (checkBox4.Checked)
+                {
+                    if (GetAsyncKeyState(Keys.XButton1) < 0)
+                    {
+                        try
+                        {
+                            if (textBox1.Text == "")
+                            {
+                                MessageBox.Show("No Text!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            };
+                            if (checkBox1.Checked)
+                            {
+                                MessageBox.Show("Another spammer is active(1)!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            };
+                            if (checkBox2.Checked)
+                            {
+                                MessageBox.Show("Another spammer is active(2)!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            };
+                            if (checkBox3.Checked)
+                            {
+                                MessageBox.Show("Another spammer is active(3)", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            }
+                            else
+                            {
+                                SendKeys.SendWait("{ENTER}" + textBox1.Text + "{ENTER}");
+                                Thread.Sleep(150);
+                            }
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+                }
+                Thread.Sleep(10);
+            }
+        }
+
+        //Credits
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var url = "https://discord.gg/xP6fw8h93P";
+            var process = new System.Diagnostics.ProcessStartInfo();
+            process.UseShellExecute = true;
+            process.FileName = url;
+            System.Diagnostics.Process.Start(process);
+        }
+    }
+}
